3315f328b20a69367bc241ac52474359073f06fc
Boris Romanov <borisromanov@Boriss-Mac-mini.local>
2016-02-14 12:35:03 +0200

    base url changed

--- TestProject/app/src/main/java/boris/test/testproject/ApiManager/Constants.java
+++ TestProject/app/src/main/java/boris/test/testproject/ApiManager/Constants.java
@@ -2,6 +2,6 @@ package boris.test.testproject.ApiManager;
 
 public class Constants {
 
-	public static String BASE_URL = "http://bebetrack-com-x9ho1okgo2pg.runscope.net/";
+	public static String BASE_URL = "http://bebetrack.com/";
 
 }
